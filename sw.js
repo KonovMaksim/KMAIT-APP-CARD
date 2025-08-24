1596f49f321c8dd2f78c8510dc8ff62d4f4977d7
KonovMaksim <60104322+KonovMaksim@users.noreply.github.com>
2025-08-24 12:59:11 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,10 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        return cache.addAll(['./', './index.html']);
+        return cache.addAll([
+          './',
+          './index.html'
+        ]);
       })
   );
 });
